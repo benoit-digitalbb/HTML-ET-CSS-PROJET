0cb698972198a0f535444f322b5191ea7981bfe9
benoit-digitalbb <benoit.baverel@digitalbb.fr>
2022-07-15 13:53:35 +0200

    first commit

---
+++ navigation.php
@@ -0,0 +1,32 @@
+<nav class="navbar fixed-top navbar-expand-lg" style="background-color: #dbf5ff;">
+            <div class="container">
+              <a class="navbar-brand" aria-current="page" href="index.html">
+                <img src="img/220502_DC_PREDIXIT_LOGO_TEXTE.svg" alt="PREDIXIT LOGO" width="150" height="">
+              </a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarOffcanvasLg" aria-controls="navbarOffcanvasLg">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="offcanvas offcanvas-end" tabindex="-1" id="navbarOffcanvasLg" aria-labelledby="navbarOffcanvasLgLabel">
+                <div class="offcanvas-header">
+                  <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Navigation</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link " href="#">Expertise préalable</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Maintenance</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link">Production</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Comptabilité</a>
+                  </li>
+                </ul>
+                </div>
+              </div>
+            </div>
+          </nav>
